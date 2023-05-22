dfe6235953aa3d57f709d1139bc4579c79e6ff2f
AseanK <seank8809@gmail.com>
2023-05-22 10:50:19 +0900

    :rice_ball: update

--- hangman/README.md
+++ hangman/README.md
@@ -5,18 +5,11 @@
 `-'     `--'   `-'  `-' `-' `----' `-' `-'    `---' `-'  `-'`-' ` `-'`----'`----' 
 ```
 
-# BlackJack
+# Hang Man
 
 ## Rules
-1. Player and dealer draws two card from the deck
-2. Dealer only shows one of the card
-3. Player has to choose either hit (draw another card) or stand (continue without drawing)
-4. Dealer will draw card until the total exceeds 17
-5. The hand with the highest totals wins as long as it doesn't exceed 21
-
-
-- Ace will represent 11 or 1, King
-- King, Queen, and Jack represents 10
+1. Guess the secret word before the stick figure is hung
+2. Player has 6 lives 
 
 ## How to play
 1. Fork the repo by clicking the fork logo on top right
@@ -26,8 +19,9 @@
    ```
    git clone git@github.com:AseanK/beginner-python-games.git
    ```
-3. Head to the blackjack folder
+3. Head to the hangman folder
 4. Run the file using python command
    ```
-   python main.py
+   python hangman.py
    ```
+
--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,32 +1,61 @@
 import random
+from asciiart import *
+import os
+from sys import platform
 
-wordList = ["glide", "coffee", "chocolate"]
+# Clears command line
+def clear():
+    if platform == "linux" or platform == "linux2":
+        # linux
+        os.system("clear")
+    elif platform == "darwin":
+        # OS X
+        os.system("clear")
+    elif platform == "win32":
+        # Windows...
+        os.system("CLS")
 
-word = random.choice(wordList)
+word = random.choice(word_list)
 tries = 6
 blank = []
+word_length = len(word)
+
+clear()
+print(logo)
 
 for i in word:
     blank += "_"
 
 print(blank)
-
 gameOver = False
 while not gameOver:
-    inp = input("Choose: ")
-    for i in range(len(word)):
-        if inp == word[i]:
-            blank[i] = word[i]
+    guess = input("Guess a letter: ").lower()
 
-    print(blank)
-    if inp not in word:
+    if guess in blank:
+        print(f"You've already guessed {guess}")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            blank[position] = letter
+
+    #Check if user is wrong.
+    if guess not in word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
         tries -= 1
-        print(f"remaining tries = {tries}")
+        if tries == 0:
+            gameOver = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(blank)}")
+
+    #Check if user has got all letters.
     if "_" not in blank:
         gameOver = True
-        print("you win")
-    if tries < 1:
-        gameOver = True
-        print('you lose')
+        print("You win.")
 
-
+    print(stages[tries])
