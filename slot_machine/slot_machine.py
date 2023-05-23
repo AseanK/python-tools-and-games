8b40231ff7ea9b487a2fcbc8e2781eeba9862aea
AseanK <seank8809@gmail.com>
2023-05-23 15:22:08 +0900

    :lobster:slot_machine

---
+++ slot_machine/slot_machine.py
@@ -0,0 +1,18 @@
+import random
+from time import sleep
+
+number_of_symbol = {
+    "★": 3,
+    "♠️": 6,
+    "♥️": 9,
+    "♦️": 12,
+    "♣️": 15
+}
+
+value_of_symbol = {
+    "★": 5,
+    "♠️": 4,
+    "♥️": 3,
+    "♦️": 2,
+    "♣️": 1
+}
