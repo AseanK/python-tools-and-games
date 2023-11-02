b33564ee8d3dabab60a3b7abdaca067b6042e136
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2023-11-02 11:59:51 +0100

    Pizza Palour

---
+++ Games/pizza_palour/README.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <a href="https://github.com/AseanK/beginner-python-games" target="_blank">
+    <img src="../../images/logo.png" width = "2560px" height = "200px">
+  </a>
+</p>
+
+# Pizza Palour
+<!-- Game Rules -->
+## Rules
+1. The game is played by a single player.
+2. The game presents a scenario where one goes to order a pizza
+3. The player is comes to order for a pizza and and is then prompted to choose the options for the pizza
+4. The player then choses the size, topping, flavour etc of his preferred pizza,
+5. After choosing, the player is then informed of his bill, if he pays, his order becomes valid
+6. If not, its Game Over!
+
+## How to play
+1. Fork the repo by clicking the fork logo the on top right <img src="../../images/fork.png" width="300" height="60">
+2. Clone the repo `git clone git@github.com:AseanK/beginner-python-games.git`
+3. Open the 'pizza_palour' folder
+4. Run the file using python command `python main.py`
+5. Follow the prompts in the console to play the game. 
+
+### Enjoy playing Treasure Island!
---
+++ Games/pizza_palour/main.py
@@ -0,0 +1,57 @@
+# Congratulations! you just got a job at python pizza,
+# Your first job is to build an automatic pizza orderin system
+# Based on user's order, work out the final bill
+
+# Pepperoni for small pizza = $2
+# Pepperoni for large and medium pizza = $3
+# Extra cheese for any size = $1
+
+# Example input:
+# size = L
+# add_pepperoni = "Y"
+# extra_cheese = "N"
+
+greetings = ("Welcome to Python Pizza ")
+name = (input("What is your name please? "))
+print(greetings + name + "!")
+
+# Small_pizza = 15 #small_pizza
+# Medium_pizza = 20 #medium_pizza
+# Large_pizza = 25 #Large_pizz
+
+
+order = (input("What size of pizza would you like to have?\nWe have the following options:\nS for Small Pizza\nM for Medium-sized Pizza\nand L for Large pizza? "))
+print(order)
+
+if order == "S":
+    bill = 15
+    print("Please pay $15 for yor order")
+elif order == "M":
+    bill = 20
+    print("Please pay $20 for yor order")
+elif order == "L":
+    bill = 25
+    print("Please pay $25 for your order")
+
+wants_pepperoni = (input("Do you want pepperoni Y or N ? "))
+if wants_pepperoni == "Y":
+    size_pepperoni = (input("Which size of pepperoni do you want: Small, Medium or Large? "))
+    print(size_pepperoni + "! " + "That's Excellent " + name)
+    if size_pepperoni == "Small":
+        bill += 2
+        print(f"Your total bill is ${bill}")
+    if size_pepperoni == "Medium":
+        bill += 3
+        print(f"Your total bill is ${bill}")
+    if size_pepperoni == "Large":
+        bill += 3
+        print(f"Your total bill is ${bill}")
+else:
+    print(f"Thank you for your order {name}; Your bill remains {bill}.\nPlease pay ${bill} to validate your order!")
+
+extra_cheese = (input("Do you want extra cheese Y or N?"))
+if extra_cheese == "Y":
+    bill += 1
+    print(f"Your Total bill is ${bill}")
+else:
+    print(f"Thank you for your order {name},\n Please pay {bill} to validate your order!")
