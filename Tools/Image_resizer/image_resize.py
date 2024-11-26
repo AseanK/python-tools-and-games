2fbc5669375285139a4ef63ceb3e866c1dc06ef4
Jemineia <118803136+Jemineia@users.noreply.github.com>
2024-11-26 19:27:13 +0900

    Update image_resize.py
    Add Webp Extension

--- Tools/Image_resizer/image_resize.py
+++ Tools/Image_resizer/image_resize.py
@@ -13,12 +13,28 @@ def reduce_image_size(image_path, quality=85):
     elif ext == '.png':
         img = img.convert("P", palette=Image.ADAPTIVE, colors=256)
         img.save(output_path, "PNG", optimize=True)
+    elif ext == '.webp':
+        if getattr(img, "is_animated", False):  # Is file Animate?
+            frames = []
+            for frame in range(img.n_frames):
+                img.seek(frame)
+                frames.append(img.copy())
+            frames[0].save(
+                output_path,
+                format="WEBP",
+                save_all=True,
+                append_images=frames[1:],
+                quality=quality,
+                duration=img.info.get("duration", 100),
+                loop=img.info.get("loop", 0)
+            )
+        else:
+            img.save(output_path, "WEBP", quality=quality)
     else:
         print("We can't resize this extension.")
         return
 
     print(f"{output_path} resize complete!")
-
 
 image_path = input("Input image path: ")
 reduce_image_size(image_path, quality=70)
